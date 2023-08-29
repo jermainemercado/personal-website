a5fe221782c0c18da397469eb125c82bf3194737
mochan <jermaine.mercado7@gmail.com>
2023-08-29 19:45:46 -0400

    fix: flash of broken CSS on load

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
-import Head from 'next/head'
-import './globals.css'
+import Head from 'next/head';
+import StyledComponentsRegistry from '@/lib/registry';
+
+import './globals.css';
 
 export const metadata = {
   title: 'jermaine mercado',
@@ -10,12 +12,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        {/* <link rel "icon" href="/favicon.ico" sizes="any" /> */}
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>"/>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>" />
         <link href="https://fonts.cdnfonts.com/css/helvetica-neue-5" rel="stylesheet" />
         <link rel="stylesheet" href="globals.css" />
       </Head>
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
